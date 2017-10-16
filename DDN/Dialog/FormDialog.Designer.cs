6ddf363a2183b21705547b5db04fdb7683d6cc2d
家君 <591821945@qq.com>
2017-10-16 13:18:57 +0800

    聊天面板可以调整输入区域大小。

--- DDN/Dialog/FormDialog.Designer.cs
+++ DDN/Dialog/FormDialog.Designer.cs
@@ -40,13 +40,18 @@
             this.labelChat = new System.Windows.Forms.Label();
             this.labelRes = new System.Windows.Forms.Label();
             this.labelDraw = new System.Windows.Forms.Label();
-            this.groupMemberPanel1 = new Dialog.GroupMemberPanel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.Rich_Edit = new Thedog.PicRichTextBox();
+            this.groupMemberPanel1 = new Dialog.GroupMemberPanel();
             this.panelChat.SuspendLayout();
             this.panelRight.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAd)).BeginInit();
             this.panelTakeEdit.SuspendLayout();
             this.flowLayoutPanelTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelChat
@@ -55,9 +60,8 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panelChat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(204)))), ((int)(((byte)(213)))));
-            this.panelChat.Controls.Add(this.richTextBoxChat);
+            this.panelChat.Controls.Add(this.splitContainer1);
             this.panelChat.Controls.Add(this.panelRight);
-            this.panelChat.Controls.Add(this.panelTakeEdit);
             this.panelChat.Location = new System.Drawing.Point(0, 35);
             this.panelChat.Name = "panelChat";
             this.panelChat.Size = new System.Drawing.Size(700, 566);
@@ -71,11 +75,11 @@
             this.richTextBoxChat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(204)))), ((int)(((byte)(213)))));
             this.richTextBoxChat.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBoxChat.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.richTextBoxChat.Location = new System.Drawing.Point(15, 0);
+            this.richTextBoxChat.Location = new System.Drawing.Point(19, 0);
             this.richTextBoxChat.Name = "richTextBoxChat";
             this.richTextBoxChat.ReadOnly = true;
             this.richTextBoxChat.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBoxChat.Size = new System.Drawing.Size(485, 430);
+            this.richTextBoxChat.Size = new System.Drawing.Size(482, 413);
             this.richTextBoxChat.TabIndex = 3;
             this.richTextBoxChat.Text = "";
             // 
@@ -105,15 +109,14 @@
             // 
             // panelTakeEdit
             // 
-            this.panelTakeEdit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.panelTakeEdit.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.panelTakeEdit.Controls.Add(this.labelTip);
             this.panelTakeEdit.Controls.Add(this.Rich_Edit);
             this.panelTakeEdit.Controls.Add(this.buttonSend);
-            this.panelTakeEdit.Location = new System.Drawing.Point(0, 433);
+            this.panelTakeEdit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelTakeEdit.Location = new System.Drawing.Point(0, 0);
             this.panelTakeEdit.Name = "panelTakeEdit";
-            this.panelTakeEdit.Size = new System.Drawing.Size(500, 133);
+            this.panelTakeEdit.Size = new System.Drawing.Size(501, 149);
             this.panelTakeEdit.TabIndex = 0;
             // 
             // labelTip
@@ -121,7 +124,7 @@
             this.labelTip.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.labelTip.AutoSize = true;
             this.labelTip.ForeColor = System.Drawing.Color.Red;
-            this.labelTip.Location = new System.Drawing.Point(298, 111);
+            this.labelTip.Location = new System.Drawing.Point(299, 127);
             this.labelTip.Name = "labelTip";
             this.labelTip.Size = new System.Drawing.Size(113, 12);
             this.labelTip.TabIndex = 3;
@@ -130,7 +133,7 @@
             // buttonSend
             // 
             this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonSend.Location = new System.Drawing.Point(417, 105);
+            this.buttonSend.Location = new System.Drawing.Point(418, 121);
             this.buttonSend.Name = "buttonSend";
             this.buttonSend.Size = new System.Drawing.Size(75, 23);
             this.buttonSend.TabIndex = 0;
@@ -188,14 +191,28 @@
             this.labelDraw.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.labelDraw.Click += new System.EventHandler(this.labelDraw_Click);
             // 
-            // groupMemberPanel1
+            // splitContainer1
             // 
-            this.groupMemberPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupMemberPanel1.Location = new System.Drawing.Point(0, 218);
-            this.groupMemberPanel1.Name = "groupMemberPanel1";
-            this.groupMemberPanel1.Size = new System.Drawing.Size(200, 348);
-            this.groupMemberPanel1.TabIndex = 1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBoxChat);
+            this.splitContainer1.Panel1MinSize = 140;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panelTakeEdit);
+            this.splitContainer1.Panel2MinSize = 140;
+            this.splitContainer1.Size = new System.Drawing.Size(501, 566);
+            this.splitContainer1.SplitterDistance = 416;
+            this.splitContainer1.SplitterWidth = 1;
+            this.splitContainer1.TabIndex = 4;
             // 
             // Rich_Edit
             // 
@@ -205,10 +222,19 @@
             this.Rich_Edit.Location = new System.Drawing.Point(0, 25);
             this.Rich_Edit.Name = "Rich_Edit";
             this.Rich_Edit.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.Rich_Edit.Size = new System.Drawing.Size(500, 78);
+            this.Rich_Edit.Size = new System.Drawing.Size(501, 94);
             this.Rich_Edit.TabIndex = 0;
             this.Rich_Edit.Text = "";
             this.Rich_Edit.TextChanged += new System.EventHandler(this.Rich_Edit_TextChanged);
+            // 
+            // groupMemberPanel1
+            // 
+            this.groupMemberPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupMemberPanel1.Location = new System.Drawing.Point(0, 218);
+            this.groupMemberPanel1.Name = "groupMemberPanel1";
+            this.groupMemberPanel1.Size = new System.Drawing.Size(200, 348);
+            this.groupMemberPanel1.TabIndex = 1;
             // 
             // FormDialog
             // 
@@ -230,6 +256,10 @@
             this.panelTakeEdit.ResumeLayout(false);
             this.panelTakeEdit.PerformLayout();
             this.flowLayoutPanelTop.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -248,5 +278,6 @@
         private System.Windows.Forms.PictureBox pictureBoxAd;
         public GroupMemberPanel groupMemberPanel1;
         private System.Windows.Forms.RichTextBox richTextBoxChat;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
